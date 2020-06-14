1bd88e36764a34cfc581f08884d90cf00c63b3c2
unknown <juxnmxgxmbox@gmail.com>
2020-06-13 21:42:13 -0500

    cuarto

---
+++ src/app/modelo/Persona.ts
@@ -0,0 +1,10 @@
+export class Persona{
+  constructor() {
+  }
+  id:number;
+  name:String;
+  apellidos:String;
+  documento:integer;
+  email:String;
+  date:String;
+}
--- src/app/restapi.service.ts
+++ src/app/restapi.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Persona } from "../modelo/Persona";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class RestapiService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  Url = 'http://localhost:8080/ejemplo/personas';
+
+  getPersonas(){
+    return this.http.get<Persona[]>(this.Url);
+  }
+
 }
